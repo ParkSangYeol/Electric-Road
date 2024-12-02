d6ebf57d61dc53edce330587ee7d6763ad927b0f
Park Sangyeol <tkddufdl77@ajou.ac.kr>
2024-12-02 17:18:44 +0900

    Fix: Check stage in inspector
    스테이지 데이터를 확인할 때 에디터에서 확인할 수 있도록 클래스 작성.

--- Assets/Scripts/ScriptableObjects/TileScriptableObject.cs
+++ Assets/Scripts/ScriptableObjects/TileScriptableObject.cs
@@ -5,6 +5,11 @@ using Sirenix.Utilities;
 using UnityEditor;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using Sirenix.OdinInspector.Editor.Drawers;
+using Sirenix.Utilities.Editor;
+#endif
+
 namespace ScriptableObjects.Stage
 {
     public class TileScriptableObject : UnityEngine.ScriptableObject
@@ -35,4 +40,44 @@ namespace ScriptableObjects.Stage
             };
         }
     }
+    
+#if UNITY_EDITOR
+    internal sealed class TileStructCellDrawer<TArray> : TwoDimensionalArrayDrawer<TArray, TileStruct>
+        where TArray : System.Collections.IList
+    {
+        protected override TableMatrixAttribute GetDefaultTableMatrixAttributeSettings()
+        {
+            return new TableMatrixAttribute()
+            {
+                SquareCells = true,
+                HideColumnIndices = true,
+                HideRowIndices = true,
+                ResizableColumns = false
+            };
+        }
+
+        protected override TileStruct DrawElement(Rect rect, TileStruct value)
+        {
+            var id = DragAndDropUtilities.GetDragAndDropId(rect);
+            
+            value = DragAndDropUtilities.DropZone<TileStruct>(rect, value);
+            value = DragAndDropUtilities.DragZone<TileStruct>(rect, value, false, false);
+            
+            DragAndDropUtilities.DrawDropZone(rect, value?.tile.Icon, null, id); 
+            
+            return value;
+        }
+
+        protected override void DrawPropertyLayout(GUIContent label)
+        {
+            base.DrawPropertyLayout(label);
+
+            // Draws a drop-zone where we can destroy items.
+            var rect = GUILayoutUtility.GetRect(0, 40).Padding(2);
+            var id = DragAndDropUtilities.GetDragAndDropId(rect);
+            DragAndDropUtilities.DrawDropZone(rect, null as UnityEngine.Object, null, id);
+            DragAndDropUtilities.DropZone<TileStruct>(rect, null, false, id);
+        }
+    }
+#endif
 }
